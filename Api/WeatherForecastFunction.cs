48d395494bd81fa59a81cc31b67d57f884264283
Erik Ljung <erikljung@gmail.com>
2024-10-04 20:30:42 +0200

    protected/public endpoint

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -4,54 +4,71 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 
-namespace Api
+namespace Api;
+
+public class HttpTrigger
 {
-    public class HttpTrigger
-    {
-        private readonly ILogger _logger;
+	private readonly ILogger _logger;
 
-        public HttpTrigger(ILoggerFactory loggerFactory)
-        {
-            _logger = loggerFactory.CreateLogger<HttpTrigger>();
-        }
+	public HttpTrigger(ILoggerFactory loggerFactory)
+	{
+		_logger = loggerFactory.CreateLogger<HttpTrigger>();
+	}
 
-        [Function("WeatherForecast")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
-        {
-            var randomNumber = new Random();
-            var temp = 0;
+	[Function("protected")]
+	public HttpResponseData Run([HttpTrigger(AuthorizationLevel.User, "get")] HttpRequestData req)
+	{
+		var randomNumber = new Random();
+		var temp = 0;
 
-            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = temp = randomNumber.Next(-20, 55),
-                Summary = GetSummary(temp)
-            }).ToArray();
+		var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = DateTime.Now.AddDays(index),
+			TemperatureC = temp = randomNumber.Next(-20, 55),
+			Summary = GetSummary(temp)
+		}).ToArray();
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.WriteAsJsonAsync(result);
+		var response = req.CreateResponse(HttpStatusCode.OK);
+		response.WriteAsJsonAsync(result);
 
-            return response;
-        }
+		return response;
+	}
 
-        private string GetSummary(int temp)
-        {
-            var summary = "Mild";
 
-            if (temp >= 32)
-            {
-                summary = "Hot";
-            }
-            else if (temp <= 16 && temp > 0)
-            {
-                summary = "Cold";
-            }
-            else if (temp <= 0)
-            {
-                summary = "Freezing";
-            }
+	[Function("Public")]
+	public HttpResponseData Public([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+	{
+		//var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		//{
+		//	Date = DateTime.Now.AddDays(index),
+		//	TemperatureC = temp = randomNumber.Next(-20, 55),
+		//	Summary = GetSummary(temp)
+		//}).ToArray();
 
-            return summary;
-        }
-    }
+		var result = "ok from public";
+		var response = req.CreateResponse(HttpStatusCode.OK);
+		response.WriteAsJsonAsync(result);
+
+		return response;
+	}
+
+	private string GetSummary(int temp)
+	{
+		var summary = "Mild";
+
+		if (temp >= 32)
+		{
+			summary = "Hot";
+		}
+		else if (temp <= 16 && temp > 0)
+		{
+			summary = "Cold";
+		}
+		else if (temp <= 0)
+		{
+			summary = "Freezing";
+		}
+
+		return summary;
+	}
 }
