30e49a1e43cf9439ea09d5b66cc0a9ad256099c2
Erik Ljung <erikljung@gmail.com>
2024-10-04 20:44:51 +0200

    with header key parse

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -1,4 +1,7 @@
 using System.Net;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 using BlazorApp.Shared;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -6,8 +9,47 @@ using Microsoft.Extensions.Logging;
 
 namespace Api;
 
+public class ClientPrincipal
+{
+	public string? IdentityProvider { get; set; }
+	public string? UserId { get; set; }
+	public string? UserDetails { get; set; }
+	public IEnumerable<string>? UserRoles { get; set; }
+}
+
 public class HttpTrigger
 {
+	public static ClaimsPrincipal Parse(HttpRequestData req)
+	{
+		var principal = new ClientPrincipal();
+
+		if (req.Headers.TryGetValues("x-ms-client-principal", out var header))
+		{
+			var data = header.FirstOrDefault();
+			var decoded = Convert.FromBase64String(data);
+			var json = Encoding.UTF8.GetString(decoded);
+			principal = JsonSerializer.Deserialize<ClientPrincipal>(json,
+				new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+		}
+
+		//Common auth code used on the Blazor side to
+		//get the Claims principal from ClientPrincipal
+		principal.UserRoles =
+			principal.UserRoles?.Except(new[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+		if (!principal.UserRoles?.Any() ?? true)
+		{
+			return new ClaimsPrincipal();
+		}
+
+		var identity = new ClaimsIdentity(principal.IdentityProvider);
+		identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+		identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+		identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+		return new ClaimsPrincipal(identity);
+	}
+
 	private readonly ILogger _logger;
 
 	public HttpTrigger(ILoggerFactory loggerFactory)
@@ -16,20 +58,36 @@ public class HttpTrigger
 	}
 
 	[Function("protected")]
-	public HttpResponseData Run([HttpTrigger(AuthorizationLevel.User, "get")] HttpRequestData req)
+	public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
 	{
 		var randomNumber = new Random();
 		var temp = 0;
 
-		var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-		{
-			Date = DateTime.Now.AddDays(index),
-			TemperatureC = temp = randomNumber.Next(-20, 55),
-			Summary = GetSummary(temp)
-		}).ToArray();
+		var key = "x-ms-client-principal";
 
 		var response = req.CreateResponse(HttpStatusCode.OK);
-		response.WriteAsJsonAsync(result);
+		if (req.Headers.TryGetValues(key, out IEnumerable<string> headers))
+		{
+			var header = headers?.FirstOrDefault();
+			var decoded = Convert.FromBase64String(header);
+			var json = Encoding.UTF8.GetString(decoded);
+			var result = JsonSerializer.Deserialize<ClientPrincipal>(json,
+				new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+			response.WriteAsJsonAsync(result);
+		}
+		else
+		{
+			var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = temp = randomNumber.Next(-20, 55),
+				Summary = GetSummary(temp)
+			}).ToArray();
+			response.WriteAsJsonAsync(result);
+		}
+
+
+		//response = req.CreateResponse(HttpStatusCode.OK);
 
 		return response;
 	}
